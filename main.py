0c90127bd996d59b86cb6b82c93b9a10bcd52325
Joseph Reiner <joereiner13@gmail.com>
2024-08-25 21:04:06 -0400

    Created user account functionality with DB and log-in.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, session, redirect, url_for, request, render_template
+from flask_mysqldb import MySQL
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
@@ -9,8 +11,16 @@ from spotipy.cache_handler import FlaskSessionCacheHandler
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(64)
 
+load_dotenv()
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+
+mysql = MySQL(app)
+
 client_id = "7cb865a7264c4d0b9e5b4c6f171582a5"
-client_secret = "28d55e3e086346d49b0866fb83293676"
+client_secret = os.getenv("CLIENT_SECRET")
 redirect_uri = "http://localhost:5000/callback"
 scope = "playlist-read-private playlist-modify-private"
 
@@ -23,8 +33,8 @@ sp_oauth = SpotifyOAuth(
     cache_handler=cache_handler,
     show_dialog=True
 )
-
 sp = Spotify(auth_manager=sp_oauth)
+
 
 @app.route("/")
 def home():
@@ -42,17 +52,41 @@ def profile():
     return render_template("profile.html")
 
 
-@app.route("/<playlist_name>/add_songs", methods=['GET'])
-def add_songs(playlist_name):
+@app.route("/<playlist_name>/search_songs", methods=['GET'])
+def search_songs(playlist_name):
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
     if request.method == 'GET':
         data = request.args
         search_text = data.get("song_search") if data.get("song_search") is not None else "roundabout" # else "": this is default value, could personalize based off other playlists or something
-        search_results = sp.search(q=search_text, limit=10, type="track")
+        search_results = sp.search(q=search_text, limit=20, type="track")
         #track_id = search_results["tracks"]["items"][idx]["uri"]
-    return render_template("add_songs.html", playlist_name=playlist_name, search_results=search_results)
+    return render_template("add_songs.html", playlist_name=playlist_name, search_text=search_text, search_results=search_results)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        if request.form["submit"] == "Log In":
+            username = request.form["username"]
+            password = request.form["password"]
+            result = cursor.execute(f"SELECT UserID FROM users WHERE Username='{username}' AND Password='{password}';")
+            if result > 0:
+                user_id = cursor.fetchall()[0][0]
+                print(f"User ID: {user_id}")
+            else:
+                print("ERROR")
+        elif request.form["submit"] == "Create Account":
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            username = request.form["username"]
+            password = request.form["password"]
+            cursor.execute(f"INSERT INTO users (FirstName, LastName, Username, Password) VALUES ('{first_name}', '{last_name}', '{username}', '{password}');")
+            mysql.connection.commit()
+            print("Successfully inserted into DB.")
+    return render_template("login.html")
 
 
 @app.route("/logout")
