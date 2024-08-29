de72b39a95b5ea885f1c5df8d48e93cf6c9d7af9
reiner-13 <joereiner13@gmail.com>
2024-08-28 21:33:05 -0400

    Added SQLite configuration to config.py.

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.urandom(64)
-    #MYSQL_HOST = os.getenv("MYSQL_HOST")
-    #MYSQL_USER = os.getenv("MYSQL_USER")
-    #MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    #MYSQL_DB = os.getenv("MYSQL_DB")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "sqlite:///" + os.path.join(basedir, "app.db")
