8751a12392b565db7ec491d8ef086175753a8b2d
reiner-13 <joereiner13@gmail.com>
2024-08-29 00:37:56 -0400

    Fixed database models and created shell context.

--- app/models.py
+++ app/models.py
@@ -4,35 +4,42 @@ import sqlalchemy.orm as so
 from app import db
 
 class User(db.Model):
+    __tablename__ = "user_table"
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
     username : so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     first_name : so.Mapped[str] = so.mapped_column(sa.String(64))
     last_name : so.Mapped[str] = so.mapped_column(sa.String(64))
     password_hash : so.Mapped[Optional[str]] = so.mapped_column(sa.String(265))
 
-    playlists : so.WriteOnlyMapped["UserPlaylist"] = so.relationship(
-        back_populates="users"
+    playlists : so.Mapped[list["UserPlaylist"]] = so.relationship(
+        back_populates="user"
     )
 
     def __repr__(self):
         return f"<User {self.username}>"
     
 class Playlist(db.Model):
+    __tablename__ = "playlist_table"
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
     name : so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
     spotify_id : so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
     apple_music_id : so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
 
-    users : so.WriteOnlyMapped["UserPlaylist"] = so.relationship(
-        back_populates="playlists"
+    users : so.Mapped[list["UserPlaylist"]] = so.relationship(
+        back_populates="playlist"
     )
 
     def __repr__(self):
         return f"<Playlist {self.name}>"
 
 class UserPlaylist(db.Model):
-    id : so.Mapped[int] = so.mapped_column(primary_key=True)
-    user_id : so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    playlist_id : so.Mapped[int] = so.mapped_column(sa.ForeignKey(Playlist.id), index=True)
+    __tablename__ = "userplaylist_table"
+    user_id : so.Mapped[int] = so.mapped_column(sa.ForeignKey("user_table.id"), primary_key=True)
+    playlist_id : so.Mapped[int] = so.mapped_column(sa.ForeignKey("playlist_table.id"), primary_key=True)
 
+    user : so.Mapped["User"] = so.relationship(back_populates="playlists")
+    playlist : so.Mapped["Playlist"] = so.relationship(back_populates="users")
+
+    def __repr__(self):
+        return f"<UserPlaylist U:{self.user_id}, P:{self.playlist_id}>"
 
