ed635cd6c68f2d75646cc30c05449b65efc2f275
reiner-13 <joereiner13@gmail.com>
2024-08-28 20:49:39 -0400

    Login route fixed.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, session, redirect, url_for, request, render_template, flash
-from flask_mysqldb import MySQL
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
@@ -8,14 +7,12 @@ from spotipy.cache_handler import FlaskSessionCacheHandler
 
 from app import app
 from app.forms import *
-#from user import User
 
 """Database Things"""
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
-mysql = MySQL(app)
 
 """Spotipy Things"""
 client_id = "7cb865a7264c4d0b9e5b4c6f171582a5"
@@ -36,7 +33,6 @@ sp = Spotify(auth_manager=sp_oauth)
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 
 @app.route("/callback")
@@ -63,34 +59,19 @@ def search_songs(playlist_name):
     return render_template("add_songs.html", playlist_name=playlist_name, search_text=search_text, search_results=search_results)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
     create_acc_form = CreateAccountForm()
-    cursor = mysql.connection.cursor()
 
     if login_form.validate_on_submit():
         username = request.form["username"]
         password = request.form["password"]
-        result = cursor.execute(f"SELECT * FROM users WHERE Username='{username}' AND Password='{password}';")
-        if result > 0:
-            data = cursor.fetchall()
-            user_id = data[0][0]
-            first_name = data[0][1]
-            last_name = data[0][2]
-            username = data[0][3]
-            password = data[0][4]
-            #current_user = User(user_id, first_name, last_name, username, password)
-        else:
-            print("ERROR")
     if create_acc_form.validate_on_submit():
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         username = request.form["username"]
         password = request.form["password"]
-        cursor.execute(f"INSERT INTO users (FirstName, LastName, Username, Password) VALUES ('{first_name}', '{last_name}', '{username}', '{password}');")
-        mysql.connection.commit()
-        print("Successfully inserted into DB.")
     return render_template("login.html", login_form=login_form, create_acc_form=create_acc_form)
 
 
